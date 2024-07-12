22eb6c11632f0fab2245cba3217d0b7cfc854cb0
Luiz Fernando <luizfernandoluth@gmail.com>
2024-07-12 19:00:08 -0300

    Re-structuring all code

--- a.py
+++ a.py
@@ -1,4 +1,4 @@
-from quantifyml.methods.aggregative import *
+from quantiML.methods.aggregative.classifyCountCorrect.acc import ACC
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -12,14 +12,14 @@ def fitting_predicting(quantifier, X_train, y_train, X_test):
     
     return result
 
-df = pd.read_csv("data/UWave.csv")
-#df["class"] = df["class"].replace(2, 0)
-#X = df.drop("class", axis=1)
-#Y = df["class"]
+df = pd.read_csv("data/BNG.csv")
+df["class"] = df["class"].replace(2, 0)
+X = df.drop("class", axis=1)
+Y = df["class"]
 
 
-X = df.iloc[:, :-1]
-Y = df.iloc[:, -1]
+#X = df.iloc[:, :-1]
+#Y = df.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=64)
 
@@ -29,7 +29,7 @@ print("Real proportion:")
 rp = np.round(y_test.value_counts(normalize=True), 3).to_dict()
 print(dict(sorted(rp.items())))
 
-quantifier = CC(classifier=rfc)
+quantifier = ACC(learner=rfc)
 
 start = time.time()
 result = fitting_predicting(quantifier, X_train, y_train, X_test)
--- quantiML/base.py
+++ quantiML/base.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedKFold
 
-from utils.utilities import parallel, normalize_prevalence
+from .utils.utilities import parallel, normalize_prevalence
 
 class Quantifier(ABC, BaseEstimator):
     """ Abstract Class for quantifiers """
@@ -51,7 +51,7 @@ class AggregativeQuantifier(Quantifier, ABC):
         self.binary_quantifiers = {}
     
 
-    def fit(self, X, y, learner_fitted=False, *args):
+    def fit(self, X, y, learner_fitted=False):
         self.classes = np.unique(y)
         #Binary quantification or multiclass quantification if method is multiclass it self
         if self.binary_data or self.multiclass_method:  
@@ -98,43 +98,155 @@ class AggregativeQuantifier(Quantifier, ABC):
         return self.binary_quantifiers[class_]._predict_method(X)
     
     
-    
-    
-class ClassifyCountCorrect(AggregativeQuantifier, ABC):
-    
-    
-    
     @classmethod
-    def GetScores(self, X, y, folds:int=10) -> np.ndarray:
+    def GetScores(self, X, y, folds:int=10, learner_fitted:bool=False) -> tuple:
         if isinstance(X, np.ndarray):
             X = pd.DataFrame(X)
         if isinstance(y, np.ndarray):
             y = pd.DataFrame(y)
-        
-        skf = StratifiedKFold(n_splits=folds)    
-        results = []
-        class_labels = []
-        
-        for train_index, valid_index in skf.split(X,y):
             
-            tr_data = pd.DataFrame(X.iloc[train_index])   #Train data and labels
-            tr_lbl = y.iloc[train_index]
-            
-            valid_data = pd.DataFrame(X.iloc[valid_index])  #Validation data and labels
-            valid_lbl = y.iloc[valid_index]
-            
-            self.learner.fit(tr_data, tr_lbl)
-            
-            results.extend(self.learner.predict_proba(valid_data)[:,1])     #evaluating scores
-            class_labels.extend(valid_lbl)
+        if learner_fitted:
+            probabilities = self.learner.predict_proba(X)[:, 1]
+            y_label = y
+        else:
         
-        scores = np.c_[results,class_labels]
+            skf = StratifiedKFold(n_splits=folds)    
+            probabilities = []
+            y_label = []
+            
+            for train_index, valid_index in skf.split(X,y):
+                
+                tr_data = pd.DataFrame(X.iloc[train_index])   #Train data and labels
+                tr_lbl = y.iloc[train_index]
+                
+                valid_data = pd.DataFrame(X.iloc[valid_index])  #Validation data and labels
+                valid_lbl = y.iloc[valid_index]
+                
+                self.learner.fit(tr_data, tr_lbl)
+                
+                probabilities.extend(self.learner.predict_proba(valid_data)[:,1])     #evaluating scores
+                y_label.extend(valid_lbl)
         
-        return scores
+        return y, probabilities
     
     
     
+    
+    
+class ThresholdOptimization(AggregativeQuantifier):
+    
+    
+    def __init__(self, learner: BaseEstimator, threshold:float=0.5):
+        self.learner = learner
+        self.threshold = threshold
+        self.cc_output = None
+        self.tpr = None
+        self.fpr = None
+    
+    
+    def multiclass_method(self):
+        return False
+    
+    
+    def _fit_method(self, X, y, learner_fitted:bool=False, cv_folds:int=10):
         
+        if isinstance(X, np.ndarray):
+            X = pd.DataFrame(X)
+        if isinstance(y, np.ndarray):
+            y = pd.DataFrame(y)
+            
+        if learner_fitted:
+            probabilities = self.learner.predict_proba(X)[:, 1]
+            y_label = y
+        else:
+        
+            skf = StratifiedKFold(n_splits=cv_folds)    
+            probabilities = []
+            y_label = []
+            
+            for train_index, valid_index in skf.split(X,y):
+                
+                tr_data = pd.DataFrame(X.iloc[train_index])   #Train data and labels
+                tr_lbl = y.iloc[train_index]
+                
+                valid_data = pd.DataFrame(X.iloc[valid_index])  #Validation data and labels
+                valid_lbl = y.iloc[valid_index]
+                
+                self.learner.fit(tr_data, tr_lbl)
+                
+                probabilities.extend(self.learner.predict_proba(valid_data)[:,1])     #evaluating scores
+                y_label.extend(valid_lbl)
+        
+        probabilities = np.asarray(probabilities)
+        #y_labels, probabilities = self.GetScores(X, y, folds=cv_folds, learner_fitted=learner_fitted)
+        
+        self.learner.fit(X, y)
+        
+        self.cc_output = len(probabilities[probabilities >= self.threshold]) / len(probabilities)
+        
+        thresholds, tprs, fprs = self.adjust_threshold(y_label, probabilities)
+        self.tpr, self.fpr = self.best_tprfpr(thresholds, tprs, fprs)
+        
+        return self
     
     
+    def _predict_method(self, X):
+        prevalences = {}
+        
+        if self.tpr - self.fpr == 0:
+            return self.cc_output
+        prevalence = np.clip((self.cc_output - self.fpr) / (self.tpr - self.fpr), 0, 1)
+        
+        prevalences[self.classes[1]] = prevalence
+        prevalences[self.classes[0]] = 1 - prevalence
+
+        return prevalences
     
+    
+    def count_predictions(self, y, y_pred) -> list:
+        TP = np.logical_and(y == y_pred, y == self.classes[1]).sum()
+        FP = np.logical_and(y != y_pred, y == self.classes[0]).sum()
+        FN = np.logical_and(y != y_pred, y == self.classes[1]).sum()
+        TN = np.logical_and(y == y_pred, y == self.classes[0]).sum()
+        return TP, FP, TN, FN
+    
+    
+    def get_tpr(self, TP, FP):
+        if TP + FP == 0:
+            return 0
+        return TP / (TP + FP)
+
+    def get_fpr(self, TN, FP):
+        if FP + TN == 0:
+            return 0
+        return FP / (FP + TN)
+
+
+    def adjust_threshold(self, y, probabilities:np.ndarray) -> tuple:
+        unique_scores = np.linspace(0,1,100)
+        
+        tprs = []
+        fprs = []
+        
+        for threshold in unique_scores:
+            y_pred = np.where(probabilities < threshold, 0, 1)
+            
+            TP, FP, TN, _ = self.count_predictions(y, y_pred)
+            
+            tpr = self.get_tpr(TP, FP)
+            fpr = self.get_fpr(TN, FP)   
+            
+            tprs.append(tpr)
+            fprs.append(fpr)
+        
+        #best_tpr, best_fpr = self.adjust_threshold(np.asarray(tprs), np.asarray(fprs))
+        return (unique_scores, np.asarray(tprs), np.asarray(fprs))
+    
+    
+    @abstractmethod
+    def best_tprfpr(self, thresholds:np.ndarray, tpr:np.ndarray, fpr:np.ndarray) -> float:
+        ...
+    
+    
+            
+            
