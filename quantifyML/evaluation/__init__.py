022883ff6fd02eb9a231e9a091c7f9efa7a8ab65
Luiz Fernando <luizfernandoluth@gmail.com>
2024-07-25 18:31:59 -0300

    some fix, and creating ensemble class

--- quantifyML/evaluation/__init__.py
+++ quantifyML/evaluation/__init__.py
@@ -1,2 +1,16 @@
 from .measures import *
 from .protocol import *
+
+MEASURES = {
+    "ae": absolute_error,
+    "nae": normalized_absolute_error,
+    "kld": kullback_leibler_divergence,
+    "nkld": normalized_kullback_leibler_divergence,
+    "nrae": normalized_relative_absolute_error,
+    "rae": relative_absolute_error,
+    "se": squared_error
+}
+
+
+def get_measure(measure:str):
+    return MEASURES.get(measure)
--- quantifyML/methods/__init__.py
+++ quantifyML/methods/__init__.py
@@ -1,1 +1,25 @@
 from .aggregative import *
+
+METHODS = {
+    "CC": CC,
+    "PCC": PCC,
+    "EMQ": EMQ,
+    "FM": FM,
+    "GAC": GAC,
+    "GPAC": GPAC,
+    "PWK": PWK,
+    "ACC": ACC,
+    "MAX": MAX,
+    "MS": MS,
+    "MS2": MS2,
+    "PACC": PACC,
+    "T50": T50,
+    "X": X_method,
+    "DyS": DyS,
+    "HDy": HDy,
+    "SMM": SMM,
+    "SORD": SORD,
+}
+
+def get_class(method):
+    return METHODS.get(method)
