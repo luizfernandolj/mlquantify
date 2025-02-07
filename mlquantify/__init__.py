1e81e24e40abd289e42fe62b3ec8e77558a7b154
Guilherme Bachega Gomes <guizobachegagomes@gmail.com>
2025-02-07 17:20:55 -0300

    Added support for pandas datframe OR numpy array for the posteriors train and test of mq.set_arguments method

--- mlquantify/__init__.py
+++ mlquantify/__init__.py
@@ -1,5 +1,7 @@
 "mlquantify, a Python package for quantification"
 
+import pandas
+
 from . import base
 from . import model_selection
 from . import plots
@@ -22,8 +24,8 @@ def set_arguments(y_pred=None, posteriors_train=None, posteriors_test=None,  y_l
     global ARGUMENTS_SETTED
     global arguments
     arguments["y_pred"] = y_pred
-    arguments["posteriors_train"] = posteriors_train
-    arguments["posteriors_test"] = posteriors_test
+    arguments["posteriors_train"] = posteriors_train.to_numpy() if isinstance(posteriors_train, pandas.DataFrame) else posteriors_train
+    arguments["posteriors_test"] = posteriors_test.to_numpy() if isinstance(posteriors_test, pandas.DataFrame) else posteriors_test
     arguments["y_labels"] = y_labels
     arguments["y_pred_train"] = y_pred_train
     
