0b50007d6d20ed7242752411b9efbd45d449c9cd
luizfernandolj <luizfernandoluth@gmail.com>
2025-12-15 17:37:05 -0300

    updated docs files and version release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("sphinxext"))
 
 project = 'mlquantify'
 author = 'Luiz Fernando'
-release = '0.1.8'
+release = '0.1.20'
 
 html_baseurl = "https://luizfernandolj.github.io/mlquantify/"
 
